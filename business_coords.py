38e94629b4d1a2c596f405d9be924bccdbe2951e
isidonnelly <60803783+isidonnelly@users.noreply.github.com>
2021-05-03 15:07:17 -0400

    added minimal things

--- business_coords.py
+++ business_coords.py
@@ -3,4 +3,18 @@ This file contains a dictionary of the coordinates for each of the businesses.
 """
 from graphics import *
 
-coords = {"Brew've Been Served",}
+x=1
+
+coords = {"Brew've Been Served":Point(2512,1189), 'Hallowed Grounds':Point(1948,947), 'Coffee Cameleon':Point(2124,1216),
+ 'Abila Airport':Point(152,1336), 'Kronos Pipe and Irrigation':Point(x,x), 'Nationwide Refinery':Point(1980,1179),
+ 'Maximum Iron and Steel':Point(463,946), 'Stewart and Sons Fabrication':Point(x,x),
+ 'Carlyle Chemical Inc.':Point(1889,1082), 'Coffee Shack':Point(1137,643), 'Bean There Done That':Point(861,369),
+ 'Brewed Awakenings':Point(x,x), "Jack's Magical Beans":Point(1589,826), 'Katerinais Cafe':Point(2391,1210),
+ 'Hippokampos':Point(x,x), 'Abila Zacharo':Point(x,x), 'Gelatogalore':Point(1268,1078), 'Kalami Kafenion':Point(x,x),
+ 'Ouzeri Elian':Point(1551,1285), "Guy's Gyros":Point(2381,1103), 'U-Pump':Point(1429,804), "Frydos Autosupply n' More":Point(2598,1100),
+ "Albert's Fine Clothing":Point(1064,554), "Shoppers' Delight":Point(x,x), 'Abila Scrapyard':Point(708,610),
+ "Frank's Fuel":Point(487,648), 'Chostus Hotel':Point(2233,737), 'General Grocer':Point(1088,1036), 'Kronos Mart':Point(792,857),
+ "Octavio's Office Supplies":Point(x,x), 'Roberts and Sons':Point(896,957), 'Ahaggo Museum':Point(1734,559),
+ 'Desafio Golf Course':Point(1336,163), 'Daily Dealz':Point(x,x)}
+
+ 
--- testing.py
+++ testing.py
@@ -1,9 +1,22 @@
 import pandas as pd
+from business_coords import coords
+from graphics import *
 
-cc_data = pd.read_csv(r'~/Desktop/MC2/cc_data.csv')
+cc_data = pd.read_csv(r'./Data/cc_data.csv')
 
 locations = cc_data.location.unique()
 
 print(locations)
 print(len(locations))
 
+win1 = GraphWin("Purchases", 1369.5,767)
+win1.setCoords(0,1534,2739,0)
+myImage = Image(Point(1369.5,767), './Data/MC2-tourist.png')
+myImage.draw(win1)
+
+for point in coords.values():
+    c1=Circle(point, 10)
+    c1.setFill('red')
+    c1.draw(win1)
+
+input("press ENTER")
