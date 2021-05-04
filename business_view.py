ecd652b0dc2225b1ccb41f785c0eb05d628f1f79
isidonnelly <60803783+isidonnelly@users.noreply.github.com>
2021-05-04 11:43:02 -0400

    added some business stuff

---
+++ business_view.py
@@ -0,0 +1,8 @@
+"""
+This file was made to show specific info about individual businesses.
+"""
+from graphics import *
+
+def show_info(location):
+    win1 = GraphWin(location, 1369.5,800)
+    win1.setCoords(0,1600,2739,0)
